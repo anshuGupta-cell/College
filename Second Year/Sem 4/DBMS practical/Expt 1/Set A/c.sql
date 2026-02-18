6556ced3c9a56edb4a27ba3e52a2d32b03c08a3e
Anshu Gupta <anshuguptacell@gmail.com>
2026-02-18 13:31:40 +0530

    Update c.sql

--- Second Year/Sem 4/DBMS practical/Expt 1/Set A/c.sql
+++ Second Year/Sem 4/DBMS practical/Expt 1/Set A/c.sql
@@ -1,3 +1,14 @@
+CREATE DATABASE company_db;
+\c company_db;
+
+CREATE TABLE Department (
+dno INT PRIMARY KEY,
+dname VARCHAR(50),
+empname VARCHAR(50),
+city VARCHAR(50)
+);
+
+-- 1 Write a procedure to insert values in Department table =>
 company_db=# create or replace procedure ins_dept(
 in dno int ,
 in dname varchar(50),
@@ -13,7 +24,31 @@ end;
 $$;
 CREATE PROCEDURE
 
-company_db=# call ins_dept(1, 'hr', 'amt', 'a');
+company_db=# call ins_dept(1, 'hr', 'amt', 'Pune');
 NOTICE:  inserted
 CALL
 
+
+-- 2 Write a procedure to display all employees working in ‘Pune’ city =>
+
+company_db=# CREATE OR REPLACE PROCEDURE display_employees_pune()
+LANGUAGE plpgsql AS $$
+DECLARE
+	rec RECORD;
+BEGIN
+	FOR rec IN
+		SELECT empname, dname
+		FROM Department
+		WHERE city = 'Pune'
+	LOOP
+		RAISE NOTICE 'Employee: %, Department: %',
+		rec.empname, rec.dname;
+	END LOOP;
+END;
+$$;
+
+CREATE PROCEDURE
+
+company_db=# CALL display_employees_pune();
+NOTICE:  Employee: amt, Department: hr
+CALL
